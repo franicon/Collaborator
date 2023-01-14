30efd9d7135b066a1c3a396d8daf078af031efa9
Abayomi Aluko <franicons@gmail.com>
2023-01-14 19:34:33 +0100

    feat: development
    * install db
    * add configuration needed

--- src/app/provider/mongodb/_module.ts
+++ src/app/provider/mongodb/_module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongoService } from './_service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([])],
   exports: [MongoService],
   providers: [MongoService],
 })
