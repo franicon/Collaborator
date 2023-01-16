d77388ab2a7c441ba8e1c7f528a94193310a7f95
Abayomi Aluko <franicons@gmail.com>
2023-01-16 23:25:51 +0100

    feat: development
    * setup schema
    * add configuration needed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export const Config = registerAs('', () => ({
   env: process.env.APP_ENV,
+
   database: {
     mongoUri: process.env.MONGO_URI,
   },
